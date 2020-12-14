d270e8cfb1401d2c51e21b0eed91c208c4b1aa4d
versil91 <alexandrsin91@mail.ru>
2020-12-14 13:53:59 +0300

    upd homework

--- task 1.1.py
+++ task 1.1.py
@@ -1,3 +1,10 @@
-name = 'test'
-print(name)
+per_1 = 5
+per_2 = 10
+per_3 = "i'm"
 
+print(per_1, per_2, per_3)
+
+per_4 = input('Введите слово: ')
+per_5 = int(input('Введите число: '))
+
+print(per_4, per_5)
