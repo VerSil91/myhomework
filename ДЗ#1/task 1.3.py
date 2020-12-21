09d2a17b0033b6c1350ab7148769c8af3cc64202
VerSil91 <63145852+VerSil91@users.noreply.github.com>
2020-12-21 14:48:56 +0300

    Delete task 1.3.py

--- ДЗ#1/task 1.3.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Введите число: "))
-print(num + int(str(num) + str(num)) + int(str(num) + str(num) + str(num)))
-
-# вот 2-ое решение, но мне кажется 1-ое более правильное :)
-# num = input("Введите число: ")
-# print(int(num) + int(num + num) + int(num + num + num))
