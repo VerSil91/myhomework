6e4c81d6474747d320c0906adfddbabc6577def0
versil91 <alexandrsin91@mail.ru>
2020-12-21 14:34:07 +0300

    upd homework

---
+++ ДЗ#1/task 1.3.py
@@ -0,0 +1,6 @@
+num = int(input("Введите число: "))
+print(num + int(str(num) + str(num)) + int(str(num) + str(num) + str(num)))
+
+# вот 2-ое решение, но мне кажется 1-ое более правильное :)
+# num = input("Введите число: ")
+# print(int(num) + int(num + num) + int(num + num + num))
