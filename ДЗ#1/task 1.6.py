558ad90a6ec3a78972e6da6046549db5f2818379
VerSil91 <63145852+VerSil91@users.noreply.github.com>
2020-12-21 14:49:14 +0300

    Delete task 1.6.py

--- ДЗ#1/task 1.6.py
+++
@@ -1,9 +0,0 @@
-start = int(input("Результат в 1-ый день (в км): "))
-finish = int(input("Необходимо достигнуть (в км): "))
-day = 1
-while True:
-    if start > finish:
-        break
-    start *= 1.1
-    day += 1
-print(f"на {day}-й день спортсмен достиг результата — не менее {finish} км.")
