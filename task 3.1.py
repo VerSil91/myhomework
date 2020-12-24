042610b18b099d20abc0dc201d19a107d2d5673f
VerSil91 <63145852+VerSil91@users.noreply.github.com>
2020-12-24 15:21:11 +0300

    ДЗ_№3

---
+++ task 3.1.py
@@ -0,0 +1,13 @@
+def div():
+    try:
+        arg1 = int(input("Input dividend "))
+        arg2 = int(input("Input divider "))
+        res = arg1 / arg2
+    except ValueError:
+        return 'Value error'
+    except ZeroDivisionError:
+        return "Wrong divider! You can't use zero as a divider"
+    return res
+
+
+print(f'result  {div()}')
