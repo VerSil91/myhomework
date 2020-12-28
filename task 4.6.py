b4297aa0f5ea115b1b6dc4e54e57e45ec88eb2ed
VerSil91 <63145852+VerSil91@users.noreply.github.com>
2020-12-28 15:03:02 +0300

    Update task 4.6.py

--- task 4.6.py
+++ task 4.6.py
@@ -1,9 +1,15 @@
 from itertools import count, cycle
 
-for el in count(int(input('Введите стартовое число: '))):
-    print(el)
+for el in count(int(input(f'Для выхода введите: "q" \nВведите стартовое число: '))):
+    print(el, end='')
+    quit = input()
+    if quit == 'q':
+        break
 
-
+print(f'Для выхода введите: "q" \nЗаранее определенный список: ')
 my_list = [True, 'ABC', 123, None]
 for el in cycle(my_list):
-    print(el)
+    print(el, end='')
+    quit = input()
+    if quit == 'q':
+        break
