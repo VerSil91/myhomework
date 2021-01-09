fa6c4a7ffb258f40b2a7135e0fb4855b68479c72
VerSil91 <63145852+VerSil91@users.noreply.github.com>
2021-01-09 11:09:23 +0300

    Update task 5.6.py

--- task 5.6.py
+++ task 5.6.py
@@ -1,6 +1,6 @@
 subj = {}
 
-with open('file_6.txt', 'r') as init_f:
+with open('file_6.txt', 'r', encoding="utf-8") as init_f:
     for line in init_f:
         subject, lecture, practice, lab = line.split()
         subj[subject] = int(lecture) + int(practice) + int(lab)
